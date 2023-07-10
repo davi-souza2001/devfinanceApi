3735bf631e277d47a2c6209e4643544853dafccc
Davi Souza <davisouza2001dv@gmail.com>
2023-07-10 20:22:03 -0300

    up

--- src/server.ts
+++ src/server.ts
@@ -18,4 +18,5 @@ app.register(jwt, {
 app.register(userRoutes)
 app.register(transferenceRoutes)
 
-app.listen({ port: 3333 }).then(() => console.log('Server is running on port 3333'))
+app.listen({ host: '0.0.0.0', port: process.env.PORT ? Number(process.env.PORT) : 3333 })
+    .then(() => console.log('Server is running :)'))
